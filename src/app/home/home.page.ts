78601ca717d8f2feb97a7a25e014729dc9554a3e
root <xbydani99x@gmail.com>
2020-10-05 10:26:16 +0200

    Comentario1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  num: number;
+  mayorMenor = '...';
+  numSecret: number = this.numAleatorio(0,100);
   constructor() {}
 
+  numAleatorio(a, b) {
+    return Math.round(Math.random() * (b - a) + parseInt(a, 10));
+  }
+  compruebaNumero(){
+    if(this.num)
+    {
+      if(this.numSecret < this.num)
+      {
+        this.mayorMenor = 'menor que';
+      }
+      else if(this.numSecret > this.num)
+      {
+        this.mayorMenor = 'mayor que';
+      }
+      else{
+        this.mayorMenor = '';
+      }
+    }
+  }
 }
